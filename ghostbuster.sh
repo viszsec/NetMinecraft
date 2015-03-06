dacad2fe9b2d84a80d2c7395ee9b77446f47d755
Infected Packet <infectedpacket@gmail.com>
2015-03-05 23:44:47 -0800

    Initial commit
    Initial commit of scripts

---
+++ ghostbuster.sh
@@ -0,0 +1,107 @@
+#█▀▀▀▀█▀▀▀▀▀██▀▀▀▀██▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀▓▒▀▀▀▀▀▀▀▀▀▀█▓▀ ▀▀▀██▀▀▀▀▀▀▀▀▀▓▓▀▀▀▀▀▀▀▀▀▌
+#▌▄██▌ ▄▓██▄ ▀▄█▓▄▐ ▄▓█▓▓▀█ ▄▓██▀▓██▓▄ ▌▄█▓█▀███▓▄ ▌▄█▓█ ▀ ▄▓██▀▓██▓▄ ▄█▓█▀███▄■
+#▌▀▓█▓▐▓██▓▓█ ▐▓█▓▌▐▓███▌■ ▒▓██▌  ▓██▓▌▐▓▒█▌▄ ▓██▓▌▐▓▒█▌▐ ▒▓██▌  ▓██▓▌▓▒█▌  ▓█▓▌
+#▐▓▄▄▌░▓▓█▓▐▓▌ █▓▓▌░▓▓█▓▄▄ ▓▓██▓▄▄▓█▓▓▌░▓█▓ █ ▓█▓▓▌░▓█▓ ▒ ▓▓██▓▄▄▓█▓▓▌▓█▓ ░ ▓█▓▓
+#▐▓▓█▌▓▓▓█▌ █▓▐██▓▌▐▓▒▓▌ ▄ ▐░▓█▌▄  ▀▀▀ ▐▓▓▓ ▐▌ ▀▀▀ ▐▓▓▓▄▄ ▐░▓█▌▄  ▀▀▀ ▓▓▓ ░ ██▓▓
+#▐▓▓▓█▐▓▒██  ██▓▓▓▌▐▓▓██ █▌▐▓▓▒▌▐ ███░▌▐▓▓▒▌▐ ███░▌▐▓▓▒▌  ▐▓▓▒▌▀ ███░▌▓▓▒▌  ███░
+# ▒▓▓█▌▒▓▓█▌ ▐▓█▒▒  ▒▓██▌▐█ ▒▓▓█ ▐█▓▒▒  ▒▒▓█ ▐█▓▒▒  ▒▒▓█ ▓▌▒▓▓█ ▐█▓▒▒ ▒▒▓█ ▐█▓▒▌
+#▌ ▒▒░▀ ▓▒▓▀ ▀░▒▓ ▐▌ ▓▓▓▀ █  █▒▓▀▀░█▓ ▄▌ ▒▒▓▀▀░█▓ ▄▌ ▒▒▓▀▀  █▒▓▀▀░█▓   ▒▒▓▀▀░█▀
+#█▄ ▀ ▄▄ ▀▄▄▀■ ▀ ▀▓█▄ ▀ ▄█▓█▄ ▀ ▓▄▄▄▄▄█▀ ▄▀ ▄▄▄▄▄▄█▓▄ ▀ ▄▄█▓▄▀     ▄▓▄█▄▀  ▄▄▄█▌
+#▐████▓█▀ ▄▄█▓▓███▄▄ ▀▓▓████████████▓▀▄██▄▀▓██████▓▓██▓▓▓█▀▀▄▄███▓▓▄▄▄▀▀█▓████▌
+#░░▌▓▓▌ ▄▒▒▓▓▓▀▀█████▄ ▀▒░▄ ███▓█▀▀ ▄▓█▌▐▒█▄▀▀▀█▓█████▓▓▀ ▄█████▀▀▓▓▓▒▒▄ ▐▓▓▐░░
+#▐▒▒█▌ ▐░▓▓██▌   ▀▀█▓█▓▄▄▄ ▀█▀▀ ▄▒▓██▀▀  ▀▀██▓▓▄ ▀▀█▀ ▄▄▄▓█▓█▀ ▀  ▐██▓▓░▌ ▐█▒▒▌
+# ▀▓█  ░▒▓███   ▄█▀▀▀▀▀▀▀█▓ ▄▒▓██▀    ▄▓▄     ▀▓██▓▄ ▓█▀▀▀▀▀▀▀▄█   ███▓▒░  █▓▀
+# ▄█▀  ▀▒▓██▓▌ █▓  ▄▄█▓▓▄▄▄░▒▀▀   ▄▄ ▓░▒▓▓ ▄▄    ▀▀█▓▄▄▄▓▓█▄▄ ▓ █ ▐▓██▓▒▀  ▀█▄
+#▐▓▌     ▀▓█▀▀   ▄▓▓▓█▀  ▄▀▀  ▄▄▀▀    ▀▀▀    ▀▀▄▄   ▀▀▄  ▀█▓▓▓▄▄  ▀▀█▓▀     ▐▓▌
+#██         ▄▄▒█▐▒██▀   ▐▌  ■▀   ▄▄░▒▒▓████▓▄▄   ▀■   ▐▌   ▀██▒▌█▒▄▄         ██
+#▐▒▌      ▄▓▓▓█▒▒▒▓▌     ▀▄   ▄▓▒▒▒███████████▒▓▓▄   ▄▀     ▐▓▒▒▒█▓▓▓▄      ▐▒▌
+# ▓▓▌    ▐▒██▀ ▐▒█▀       ▄▄▓▒▒▒▒▓███████████████▓▓▓▄        ▀█▒▌ ▀██▒▌    ▐▓▓
+#  ▀█    ▒▒▓▌  ▒▓▌     ▄▓▒▒▒▒█░█▓██████████████████▓▓▓▓▄      ▐▓▒  ▐▓▒▒    █▀
+# ▄█▀▀  ▐▒█▀   ▐▓▓    ▓░░▓▒▒▓▓▓████████████████████▓▓▓▓▒▓     ▓▓▌   ▀█▒▌  ▀▀█▄
+#▐▓▌    ▒▓▌   ▄▄▒█▌▄ ▐░▒▒▓▓▓▀▀   ▀█▓█████████████▓▀▀█▓▓▓▓▌  ▄▐█▒▄▄   ▐▓▒    ▐▓▌
+#██     ▐▓▓ ▄▓▓▓█▒▓▌ ░▒░░▒▀        ▓▓█████████▓▀     ▀░▓▒▓▌ ▐▓▒█▓▓▓▄ ▓▓▌     ██
+#▐▒▌     ▒█▐▒██▀  ▒▓ ▐░░▒█  ▓  ▀   ▐▓████████▓▌  ▀  █ ▐▓▓▓  ▓▒  ▀██▒▌█▒     ▐▒▌
+# ▓▓▌     ▒▒▒▓▌    ▀▒ ▀░▒▓▒▄ ▀▄▄█   █████████▓ ▐▌ ▄▀ ▄▒▓▒▌ ▒▀    ▐▓▒▒▒     ▐▓▓
+#  ▀█     ▐▒█▀▄▄        ▀▀█▓█▄▄   ▄█▒█▓▓▓▀▓████▄▀▀ ▄█▓█▒▀       ▄▄▀█▒▌     █▀
+# ▄█▀▀    ▒▓▌▒▓▓▓▄    ▄░▒▓▄▄▀▓█▓████▓▀▀    ▓███▓█████▀▄▄▄     ▄▓▓▓▒▐▓▒    ▀▀█▄
+#▐▓▌      ▐▓▓ ▀██▒▌  ▐░▒▒▒▓██▄███▓▀         ▀▓██▓██▀▄█▓▒▓▓▄  ▐▒██▀ ▓▓▌      ▐▓▌
+#██      ▒█▒█▌ ▐▓▒▒  ░░▒▒█▓▓▓███▓▀           ▀▓████▓▓█▓█▓▓█  ▒▒▓▌ ▐█▒█▒      ██
+#▐▒▌    ▐▓▓ ▒▓▌ ▀█▒▌ ▒░▒░▒░▓▓██▓▄     ▄▄▄▓▄   ▄█▒▒█▒▒▓▓▒▓▓▓▌▐▒█▀ ▐▓▒ ▓▓▌    ▐▒▌
+# ▓▓▌   ▒▓▌  ▒▓  ▐▓▒  ▀░░▒▒▒█████▓▄ ▄▓█████████▓██▒▒▒▒▓▓▓▒▓ ▒▓▌  ▓▒  ▐▓▒   ▐▓▓
+#  ▀█   ▐▒█▄  ▀▒ ▓▓▌    ▀▒░▒▓███▀▀▀▀▓█▀▀▀▀▀█▀▀▀▀▓▓█▓▒▓▓▒░▀  ▐▓▓ ▒▀  ▄█▒▌   █▀
+# ▄█▀▀   ▒▒▓▌   ▐▄▒        ▀▀ ▄▓▓ █▄▄ ▄▀▀▀▄▄▀▄▀▀▀▄ ▀▀▀▀      ▒▄▌   ▐▓▒▒   ▀▀█▄
+#▐▓▌     ▐▒██▄ ▐▒▓   ▄▄▒█▌▄  ▐▒▒▌▀▐▌▀█▌ ▀▓ ▐█▌▀▀▌▒▌  ▄▐█▒▄▄   ▓▒▌ ▄██▒▌     ▐▓▌
+#██ cXc[CPH]▓▓█▒▓▌ ▄▓▓▓█▒▓▌   ▒▒▒██▄ ▄▄▀  ▄▄█▓▓█▓▒   ▐▓▒█▓▓▓ ▄▐▓▒█▓▓▓▀       ██
+#▐▒▌        ▀▀▒█▌▀▐▒██▀ ▐▒▓    ▀▒▒▒████████▓█▓▄▀▀    ▓▒▌ ▀██▐▒▀▐█▒▀▀        ▐▒▌
+# ▓▓▌        ▐▓▓ ▒▒▒▓▌█▒▌▐▀▒      ▀░▒▒▓▓▓█▓▀▀       ▒▀▌▐▒█▐▓▒▒▒ ▓▓▌        ▐▓▓
+# ▄▀█         ▒▄▌▐▒█▀▓▒▄▒ ▓▓▌        ▀▀▀▀          ▐▓▓ ▒▄▒▓▀▒█▌▐▄▒         █▀▄
+#▐▓▌ ▀         ▓▒▌ ▄██▒▌                                ▐▒██ ▄▐▒▓         ▀ ▐▓▌
+#██            ▐▓▒█▓▓▓▀                                  ▀▓▓█▓▒▓▌            ██
+#▐▒▌    ▄▄▄▓█▀■▀▐█▒▀▀                                      ▀▒▀█▌▀■▀█▓▄▄▄    ▐▒▌
+# ▓▄▄███▓▀▀                                                          ▀▀█▓██▄▄▓
+#▄█▓▀▀                                                                    ▀▀▓█▄
+#█▓                                                                          ▓█
+#▓▒                                                                          ▒▓
+#▓                                                                            ▓
+#    Copyright (C) 2015 Jonathan Racicot
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    Description:
+#
+#		Simple script to copy a suspicious file from a given source to
+#		a repository. The suspicious file is hashed using MD5. The MD5
+#		hash is use to create a folder within the repository unless already
+#		present and files are copied to it.
+#
+#	Usage:
+#
+#		TODO
+#
+#	TODO:
+#		Use getops to parse parameters
+#!/bin/bash
+if [ $# -ne 2 ] ; then
+        echo "Usage $0 <source-directory> <dest-directory>"
+        exit 1
+fi
+
+DIR_SRC="$1*"
+DIR_DEST="$2"
+DIR_CUR=`pwd`
+FILE_HASH=$DIR_CUR/hashes.md5
+md5sum $DIR_SRC > $FILE_HASH
+while read -r signature
+do
+  echo $signature
+  MD5HASH=`echo $signature | cut -d" " -f1`
+  FILE=`echo $signature | cut -d" " -f2`
+  FILENAME=`basename $FILE`
+  DIR_FILE_DEST="$DIR_DEST/$MD5HASH/"
+  if [ -d $DIR_FILE_DEST ]; then
+    echo "[!] Directory for $MD5HASH already exists. Skipping"
+  else
+    echo "[*] Creating $DIR_FILE_DEST"
+    mkdir -p $DIR_FILE_DEST
+    echo "[*] Moving $FILENAME to $DIR_FILE_DEST"
+    mv $FILE $DIR_FILE_DEST
+    if [ -e $DIR_FILE_DEST/$FILENAME ]; then
+      echo "[+] File successfully captured."
+    else
+      echo "[-] File was not captured..."
+    fi
+  fi
+done < "$FILE_HASH"
+rm -f $FILE_HASH
+
+
---
+++ netminecraft.sh
@@ -0,0 +1,273 @@
+#█▀▀▀▀█▀▀▀▀▀██▀▀▀▀██▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀▓▒▀▀▀▀▀▀▀▀▀▀█▓▀ ▀▀▀██▀▀▀▀▀▀▀▀▀▓▓▀▀▀▀▀▀▀▀▀▌
+#▌▄██▌ ▄▓██▄ ▀▄█▓▄▐ ▄▓█▓▓▀█ ▄▓██▀▓██▓▄ ▌▄█▓█▀███▓▄ ▌▄█▓█ ▀ ▄▓██▀▓██▓▄ ▄█▓█▀███▄■
+#▌▀▓█▓▐▓██▓▓█ ▐▓█▓▌▐▓███▌■ ▒▓██▌  ▓██▓▌▐▓▒█▌▄ ▓██▓▌▐▓▒█▌▐ ▒▓██▌  ▓██▓▌▓▒█▌  ▓█▓▌
+#▐▓▄▄▌░▓▓█▓▐▓▌ █▓▓▌░▓▓█▓▄▄ ▓▓██▓▄▄▓█▓▓▌░▓█▓ █ ▓█▓▓▌░▓█▓ ▒ ▓▓██▓▄▄▓█▓▓▌▓█▓ ░ ▓█▓▓
+#▐▓▓█▌▓▓▓█▌ █▓▐██▓▌▐▓▒▓▌ ▄ ▐░▓█▌▄  ▀▀▀ ▐▓▓▓ ▐▌ ▀▀▀ ▐▓▓▓▄▄ ▐░▓█▌▄  ▀▀▀ ▓▓▓ ░ ██▓▓
+#▐▓▓▓█▐▓▒██  ██▓▓▓▌▐▓▓██ █▌▐▓▓▒▌▐ ███░▌▐▓▓▒▌▐ ███░▌▐▓▓▒▌  ▐▓▓▒▌▀ ███░▌▓▓▒▌  ███░
+# ▒▓▓█▌▒▓▓█▌ ▐▓█▒▒  ▒▓██▌▐█ ▒▓▓█ ▐█▓▒▒  ▒▒▓█ ▐█▓▒▒  ▒▒▓█ ▓▌▒▓▓█ ▐█▓▒▒ ▒▒▓█ ▐█▓▒▌
+#▌ ▒▒░▀ ▓▒▓▀ ▀░▒▓ ▐▌ ▓▓▓▀ █  █▒▓▀▀░█▓ ▄▌ ▒▒▓▀▀░█▓ ▄▌ ▒▒▓▀▀  █▒▓▀▀░█▓   ▒▒▓▀▀░█▀
+#█▄ ▀ ▄▄ ▀▄▄▀■ ▀ ▀▓█▄ ▀ ▄█▓█▄ ▀ ▓▄▄▄▄▄█▀ ▄▀ ▄▄▄▄▄▄█▓▄ ▀ ▄▄█▓▄▀     ▄▓▄█▄▀  ▄▄▄█▌
+#▐████▓█▀ ▄▄█▓▓███▄▄ ▀▓▓████████████▓▀▄██▄▀▓██████▓▓██▓▓▓█▀▀▄▄███▓▓▄▄▄▀▀█▓████▌
+#░░▌▓▓▌ ▄▒▒▓▓▓▀▀█████▄ ▀▒░▄ ███▓█▀▀ ▄▓█▌▐▒█▄▀▀▀█▓█████▓▓▀ ▄█████▀▀▓▓▓▒▒▄ ▐▓▓▐░░
+#▐▒▒█▌ ▐░▓▓██▌   ▀▀█▓█▓▄▄▄ ▀█▀▀ ▄▒▓██▀▀  ▀▀██▓▓▄ ▀▀█▀ ▄▄▄▓█▓█▀ ▀  ▐██▓▓░▌ ▐█▒▒▌
+# ▀▓█  ░▒▓███   ▄█▀▀▀▀▀▀▀█▓ ▄▒▓██▀    ▄▓▄     ▀▓██▓▄ ▓█▀▀▀▀▀▀▀▄█   ███▓▒░  █▓▀
+# ▄█▀  ▀▒▓██▓▌ █▓  ▄▄█▓▓▄▄▄░▒▀▀   ▄▄ ▓░▒▓▓ ▄▄    ▀▀█▓▄▄▄▓▓█▄▄ ▓ █ ▐▓██▓▒▀  ▀█▄
+#▐▓▌     ▀▓█▀▀   ▄▓▓▓█▀  ▄▀▀  ▄▄▀▀    ▀▀▀    ▀▀▄▄   ▀▀▄  ▀█▓▓▓▄▄  ▀▀█▓▀     ▐▓▌
+#██         ▄▄▒█▐▒██▀   ▐▌  ■▀   ▄▄░▒▒▓████▓▄▄   ▀■   ▐▌   ▀██▒▌█▒▄▄         ██
+#▐▒▌      ▄▓▓▓█▒▒▒▓▌     ▀▄   ▄▓▒▒▒███████████▒▓▓▄   ▄▀     ▐▓▒▒▒█▓▓▓▄      ▐▒▌
+# ▓▓▌    ▐▒██▀ ▐▒█▀       ▄▄▓▒▒▒▒▓███████████████▓▓▓▄        ▀█▒▌ ▀██▒▌    ▐▓▓
+#  ▀█    ▒▒▓▌  ▒▓▌     ▄▓▒▒▒▒█░█▓██████████████████▓▓▓▓▄      ▐▓▒  ▐▓▒▒    █▀
+# ▄█▀▀  ▐▒█▀   ▐▓▓    ▓░░▓▒▒▓▓▓████████████████████▓▓▓▓▒▓     ▓▓▌   ▀█▒▌  ▀▀█▄
+#▐▓▌    ▒▓▌   ▄▄▒█▌▄ ▐░▒▒▓▓▓▀▀   ▀█▓█████████████▓▀▀█▓▓▓▓▌  ▄▐█▒▄▄   ▐▓▒    ▐▓▌
+#██     ▐▓▓ ▄▓▓▓█▒▓▌ ░▒░░▒▀        ▓▓█████████▓▀     ▀░▓▒▓▌ ▐▓▒█▓▓▓▄ ▓▓▌     ██
+#▐▒▌     ▒█▐▒██▀  ▒▓ ▐░░▒█  ▓  ▀   ▐▓████████▓▌  ▀  █ ▐▓▓▓  ▓▒  ▀██▒▌█▒     ▐▒▌
+# ▓▓▌     ▒▒▒▓▌    ▀▒ ▀░▒▓▒▄ ▀▄▄█   █████████▓ ▐▌ ▄▀ ▄▒▓▒▌ ▒▀    ▐▓▒▒▒     ▐▓▓
+#  ▀█     ▐▒█▀▄▄        ▀▀█▓█▄▄   ▄█▒█▓▓▓▀▓████▄▀▀ ▄█▓█▒▀       ▄▄▀█▒▌     █▀
+# ▄█▀▀    ▒▓▌▒▓▓▓▄    ▄░▒▓▄▄▀▓█▓████▓▀▀    ▓███▓█████▀▄▄▄     ▄▓▓▓▒▐▓▒    ▀▀█▄
+#▐▓▌      ▐▓▓ ▀██▒▌  ▐░▒▒▒▓██▄███▓▀         ▀▓██▓██▀▄█▓▒▓▓▄  ▐▒██▀ ▓▓▌      ▐▓▌
+#██      ▒█▒█▌ ▐▓▒▒  ░░▒▒█▓▓▓███▓▀           ▀▓████▓▓█▓█▓▓█  ▒▒▓▌ ▐█▒█▒      ██
+#▐▒▌    ▐▓▓ ▒▓▌ ▀█▒▌ ▒░▒░▒░▓▓██▓▄     ▄▄▄▓▄   ▄█▒▒█▒▒▓▓▒▓▓▓▌▐▒█▀ ▐▓▒ ▓▓▌    ▐▒▌
+# ▓▓▌   ▒▓▌  ▒▓  ▐▓▒  ▀░░▒▒▒█████▓▄ ▄▓█████████▓██▒▒▒▒▓▓▓▒▓ ▒▓▌  ▓▒  ▐▓▒   ▐▓▓
+#  ▀█   ▐▒█▄  ▀▒ ▓▓▌    ▀▒░▒▓███▀▀▀▀▓█▀▀▀▀▀█▀▀▀▀▓▓█▓▒▓▓▒░▀  ▐▓▓ ▒▀  ▄█▒▌   █▀
+# ▄█▀▀   ▒▒▓▌   ▐▄▒        ▀▀ ▄▓▓ █▄▄ ▄▀▀▀▄▄▀▄▀▀▀▄ ▀▀▀▀      ▒▄▌   ▐▓▒▒   ▀▀█▄
+#▐▓▌     ▐▒██▄ ▐▒▓   ▄▄▒█▌▄  ▐▒▒▌▀▐▌▀█▌ ▀▓ ▐█▌▀▀▌▒▌  ▄▐█▒▄▄   ▓▒▌ ▄██▒▌     ▐▓▌
+#██ cXc[CPH]▓▓█▒▓▌ ▄▓▓▓█▒▓▌   ▒▒▒██▄ ▄▄▀  ▄▄█▓▓█▓▒   ▐▓▒█▓▓▓ ▄▐▓▒█▓▓▓▀       ██
+#▐▒▌        ▀▀▒█▌▀▐▒██▀ ▐▒▓    ▀▒▒▒████████▓█▓▄▀▀    ▓▒▌ ▀██▐▒▀▐█▒▀▀        ▐▒▌
+# ▓▓▌        ▐▓▓ ▒▒▒▓▌█▒▌▐▀▒      ▀░▒▒▓▓▓█▓▀▀       ▒▀▌▐▒█▐▓▒▒▒ ▓▓▌        ▐▓▓
+# ▄▀█         ▒▄▌▐▒█▀▓▒▄▒ ▓▓▌        ▀▀▀▀          ▐▓▓ ▒▄▒▓▀▒█▌▐▄▒         █▀▄
+#▐▓▌ ▀         ▓▒▌ ▄██▒▌                                ▐▒██ ▄▐▒▓         ▀ ▐▓▌
+#██            ▐▓▒█▓▓▓▀                                  ▀▓▓█▓▒▓▌            ██
+#▐▒▌    ▄▄▄▓█▀■▀▐█▒▀▀                                      ▀▒▀█▌▀■▀█▓▄▄▄    ▐▒▌
+# ▓▄▄███▓▀▀                                                          ▀▀█▓██▄▄▓
+#▄█▓▀▀                                                                    ▀▀▓█▄
+#█▓                                                                          ▓█
+#▓▒                                                                          ▒▓
+#▓                                                                            ▓
+#    Copyright (C) 2015 Jonathan Racicot
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    Description:
+#
+#       This scripts automate extraction of information from PCAP file
+#	using the t-shark tool provided with Wireshark. As such, you will
+#	need to have t-shark installed for this script to work;
+#
+#	sudo apt-get install tshark
+#
+#    Usage:
+#
+# 	To split a larger PCAP file into protocol-specific PCAPs, use;
+#
+#	 netminecraft.sh -s <protocol> -r <pcap_file>
+#
+#	Note that the scripts only passes the contents of <protocol> to
+#	t-shark. As such, you can specify any Wireshark filter to extract
+#	even more specific information, for example:
+#
+#	 netminecraft.sh -s http.request.full_uri -r 20150417.pcap
+#
+#	However avoid filters with spaces as the current version of the
+#	script does not manage spaces. The results will be saved in a file
+#	in the current directory as <protocol>.pcap, for example http.pcap.
+#
+#	To mine for data relating to a specific protocol, use;
+#
+#	 netminecraft.sh -p (dns|http|ssl|mail|ftp) -r <pcap_file> -w <output_file>
+#
+#	The output file will contain text data that have been sorted and in
+#	which the doubles will have been removed using 'uniq -i', i.e.
+#	we ignore the case of the items.
+#
+#	Examples:
+#
+#	 netminecraft.sh -p dns -r dns.pcap -w dns.queries.txt
+#
+#	The example above will extract the URLs of all the DNS queries found
+#	in the file dns.pcap and will output a list of URLs in dns.queries.txt:
+#
+#	 www.doctorwho.com
+#	 www.facebook.com
+#	 www.petcarservices.com
+#	 www.travelvirginia.us
+#	 updates.firefox.com
+#	 [...]
+#
+#
+#!/bin/bash
+
+function show_help {
+  echo "Netminecraft - Extract information from PCAP file for intelligence gathering."
+  echo "Usage: $0 [-s <protocol>] -p <protocol> -r <pcap_file> -w <output_file>"
+  echo ""
+  echo -e '    -s <protocol>\n\t\tSplits the provided PCAP file into a smaller file'
+  echo -e '    \t\tcontaining only traffic specified by <protocol>. '
+  echo -e '    -p (http|ftp|mail|dns|ssl|all)\n\t\tSpecify from which protocol to extract'
+  echo -e '    \t\tdata from. For example, specifying http will extract URLs'
+  echo -e '    \t\tof the http requests contained in the PCAP file.'
+}
+
+# Verify if any arguments were given
+# if not, exit
+if [ $# -eq 0 ];
+then
+    show_help
+    exit 0
+fi
+
+
+# Reset in case getopts has been used previously in the shell.
+OPTIND=1
+PCAP_FILE=""
+OUTPUT_FILE=""
+PROTOCOL=""
+PROTOSPLIT=""
+
+while getopts "h?p:r:w:s:" opt;
+do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    p)  PROTOCOL=$OPTARG
+        ;;
+    r)  PCAP_FILE=$OPTARG
+        ;;
+    w)  OUTPUT_FILE=$OPTARG
+        ;;
+    s)  PROTOSPLIT=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+[ "$1" = "--" ] && shift
+
+echo "netminecraft.sh 1.0 - Extract useful data from pcap files for"
+echo "intelligence purpose. This program relies on t-shark to function."
+echo ""
+echo "This program is distributed in the hope that it will be useful,"
+echo "but WITHOUT ANY WARRANTY; without even the implied warranty of"
+echo "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
+echo "GNU General Public License for more details."
+echo ""
+
+#
+# Verifies the existence of the PCAP file
+#
+if [ ! -f "$PCAP_FILE" ];
+then
+  echo "[-] $PCAP_FILE was not found. Please verify the location."
+  exit 66
+fi
+
+#
+# Splits the master PCAP file into a smaller
+# one as specified by the argument of the -s
+# option.
+#
+if [ -n "$PROTOSPLIT" ];
+then
+  echo "[*] Splitting $PCAP_FILE. This may take a while..."
+  PROTO=$PROTOSPLIT
+  echo "[*] Extracting $PROTO traffic..."
+  tshark -n -2 -Q -r "$PCAP_FILE" -R "$PROTO" -w "$PROTO".pcap
+  echo "[+] Completed."
+  exit 0
+fi
+
+#
+# Create the output file if it doesn't exist
+#
+if [ ! -f "$OUTPUT_FILE" ];
+then
+	touch "$OUTPUT_FILE"
+fi
+
+echo "[*] Network data extraction starting on $PCAP_FILE..."
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Extract information about the FTP sessions
+#
+if [ "$PROTOCOL" = "ftp" ] || [ "$PROTOCOL" = "all" ];
+then
+  NUMOFLINES_BEFORE=$(wc -l < "$OUTPUT_FILE")
+  echo "[*] Extracting FTP data..."
+  echo "[*] Extracting logins and uploads..."
+  tshark -n -2 -r "$PCAP_FILE" -T fields -E header=n -E separator=, -E occurrence=a -e ftp.request.arg -R 'ftp.request.command=="STOR" || ftp.request.command=="USER"' | sort | uniq -i >> "$OUTPUT_FILE"
+  NUMOFLINES_AFTER=$(wc -l < "$OUTPUT_FILE")
+  NEWDATA=$(($NUMOFLINES_AFTER-$NUMOFLINES_BEFORE))
+  echo "[+] Extraction completed. $NEWDATA line(s) added to $OUTPUT_FILE."
+fi
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Extract information about the HTTP sessions
+#
+if [ "$PROTOCOL" = "http" ] || [ "$PROTOCOL" = "all" ];
+then
+  NUMOFLINES_BEFORE=$(wc -l < "$OUTPUT_FILE")
+  echo "[*] Extracting HTTP data..."
+  echo "[*] Extracting URLs from HTTP Requests..."
+  tshark -n -2 -r "$PCAP_FILE" -T fields -e http.request.full_uri -R 'http' | sort | uniq -i >> "$OUTPUT_FILE"
+  NUMOFLINES_AFTER=$(wc -l < "$OUTPUT_FILE")
+  NEWDATA=$(($NUMOFLINES_AFTER-$NUMOFLINES_BEFORE))
+  echo "[+] Extraction completed. $NEWDATA line(s) added to $OUTPUT_FILE."
+fi
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Extract information about the DNS Queries
+#
+if [ "$PROTOCOL" = "dns" ] || [ "$PROTOCOL" = "all" ];
+then
+  NUMOFLINES_BEFORE=$(wc -l < "$OUTPUT_FILE")
+  echo "[*] Extracting URLs from DNS Queries..."
+  tshark -n -2 -r "$PCAP_FILE" -T fields -e dns.qry.name -R 'dns' | sort | uniq -i >> "$OUTPUT_FILE"
+  NUMOFLINES_AFTER=$(wc -l < "$OUTPUT_FILE")
+  NEWDATA=$(($NUMOFLINES_AFTER-$NUMOFLINES_BEFORE))
+  echo "[+] Extraction completed. $NEWDATA line(s) added to $OUTPUT_FILE."
+fi
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Extract information about mail
+#
+if [ "$PROTOCOL" = "mail" ] || [ "$PROTOCOL" = "all" ];
+then
+  NUMOFLINES_BEFORE=$(wc -l < "$OUTPUT_FILE")
+  echo "[*] Extracting email addresses from mail traffic..."
+tshark -n -2 -r "$PCAP_FILE" -T fields -E header=y -E separator=, -E occurrence=a -E aggregator =\| -e smtp.req.parameter -R 'smtp.req.command=="RCPT" || smtp.req.command=="MAIL"' | sort | uniq -i >> "$OUTPUT_FILE"
+  NUMOFLINES_AFTER=$(wc -l < "$OUTPUT_FILE")
+  NEWDATA=$(($NUMOFLINES_AFTER-$NUMOFLINES_BEFORE))
+  echo "[+] Extraction completed. $NEWDATA line(s) added to $OUTPUT_FILE."
+fi
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Extract information about the SSL certificates
+#
+if [ "$PROTOCOL" = "ssl" ] || [ "$PROTOCOL" = "all" ];
+then
+  NUMOFLINES_BEFORE=$(wc -l < "$OUTPUT_FILE")
+  echo "[*] Extracting SSL certificates..."
+  tshark -n -r "$PCAP_FILE" "ssl.handshake.certificate" -T fields -E header=y -E separator=, -E occurrence=a -E aggregator =\| -e x509sat.CountryName -ex509sat.IA5String -e x509.printableString -e x509sat.teletexString -e x509sat.UTF8String -e x509sat.universalString | sort | uniq >> "$OUTPUT_FILE"
+  NUMOFLINES_AFTER=$(wc -l < "$OUTPUT_FILE")
+  NEWDATA=$(($NUMOFLINES_AFTER-$NUMOFLINES_BEFORE))
+  echo "[+] Extraction completed. $NEWDATA line(s) added to $OUTPUT_FILE."
+fi
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+echo "[*] Network data extraction completed. Program terminating."
+exit
+
+
